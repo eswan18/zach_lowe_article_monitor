81c3b6d13806710190f3e972032c4fb2a7ab0996
Ethan Swan <eswan18@rocketmail.com>
2017-06-14 04:13:49 +0000

    successfully have list of article links

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ def main():
     auth.set_access_token(creds['access_token'], creds['access_secret'])
     api = tw.API(auth)
 
-    # Start scraping
+    # Scraping tweets
     lowe_tweets = gtfu.get_tweets_for_user('ZachLowe_NBA', api)
-    print lowe_tweets[2]
+
+    # Find URLs of Zach's articles
+    zl_articles = []
+    for tweet in lowe_tweets:
+        for url in tweet.entities['urls']:
+            link = url['expanded_url']
+            if "espn.com" in link and "story" in link:
+                zl_articles.append(link)
+    # Return the uniq article links
+    return list(set(zl_articles))
 
 if __name__ == "__main__":
     main()
