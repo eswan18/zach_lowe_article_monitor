e9a5b145ef6ad6c9d1a08a9c39691c1e3c186e21
Ethan Swan <eswan18@rocketmail.com>
2017-06-14 03:08:56 +0000

    just beginning...

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import pickle
+
+def main():
+    # Import the predefined credentials dictionary
+    creds = pickle.load(open("creds.pickle"))
+    
+    
+
+if __name__ == "__main__":
+    main()
